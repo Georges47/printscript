6a5dbf509ea43dab4a72a0a16047c62cc6609b9f
Georges Stefan <stefangeorges4799@gmail.com>
2022-03-09 18:26:19 -0300

    Reads file path from user and prints its content in uppercase

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,11 @@
-object Main extends App {
-  println("Hello, World!")
-  println("Hello Dimi From Lucas")
+import scala.io.{Source, StdIn}
 
+object Main extends App {
+  print("Enter the absolute path of the text file: ")
+  val pathToFile = StdIn.readLine()
+  val bufferedSource = Source.fromFile(pathToFile)
+  for (line <- bufferedSource.getLines) {
+    println(line.toUpperCase)
+  }
+  bufferedSource.close
 }
