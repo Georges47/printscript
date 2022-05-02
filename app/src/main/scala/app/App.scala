c4b9f63d0a55d3d495f960ce6c70aaa64909e51f
Georges Stefan <stefangeorges4799@gmail.com>
2022-05-02 20:30:46 -0300

    Modifies App so that it can be used in java code

--- app/src/main/scala/app/App.scala
+++ app/src/main/scala/app/App.scala
@@ -1,12 +1,12 @@
 package app
 
 import abstractSyntaxTree.AbstractSyntaxTree
-import interpreter.Interpreter
+import interpreter.{IdentifierTable, Interpreter}
 import lexer.Lexer
 import org.austral.ingsis.printscript.common.Token
 import parser.Parser
 
-object App {
+class App {
   def lex(content: String): List[Token] = {
     val lexer = new Lexer(content)
     lexer.lex
@@ -20,7 +20,7 @@ object App {
 
   def interpret(content: String): Unit = {
     val ast = parse(content)
-    val interpreter = new Interpreter
+    val interpreter = new Interpreter(IdentifierTable(), IdentifierTable())
     interpreter.interpret(ast)
   }
 }
